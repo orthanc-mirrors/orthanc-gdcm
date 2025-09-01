14eba7708df7c180cd7787e2d9f7ffa392193330
Sebastien Jodogne <s.jodogne@gmail.com>
2025-09-01 18:48:56 +0200

    cont

--- Resources/CMake/GdcmConfiguration.cmake
+++ Resources/CMake/GdcmConfiguration.cmake
@@ -103,8 +103,8 @@ if (STATIC_BUILD OR NOT USE_SYSTEM_GDCM)
     CMAKE_ARGS -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE} "-DCMAKE_INSTALL_PREFIX=${GDCM_INSTALL_DIR}" ${Flags}
 
     # https://stackoverflow.com/a/43363395
-    # The "--config" option seems not available for MSVC
-    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install # --config ${CMAKE_BUILD_TYPE}
+    # The "--config" option seems not available on old versions of CMake
+    INSTALL_COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CMAKE_BUILD_TYPE}
     )
 
   if(MSVC)
